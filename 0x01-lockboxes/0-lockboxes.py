1fd2170c61d26e744dd1d50de69de845f4f7375e
hadiglitch10 <hadiunknownglitch@gmail.com>
2024-07-06 15:39:51 +0300

    besm allah

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A method that determines if all the boxes can be opened."""
+
+
+def canUnlockAll(boxes):
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
+    """
+    n = len(boxes)
+    unlocked = set([0])  # Start with the first box unlocked
+    keys = [0]           # Use a list as a queue to manage the boxes we can visit
+    
+    while keys:
+        current_box = keys.pop()
+        for key in boxes[current_box]:
+            if key not in unlocked and key < n:
+                unlocked.add(key)
+                keys.append(key)
+    
+    return len(unlocked) == n
