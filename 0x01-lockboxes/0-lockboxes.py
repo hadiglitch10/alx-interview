615b2a0502356b0a8535b271ad99756942a26cbe
hadiglitch10 <hadiunknownglitch@gmail.com>
2024-07-06 15:52:52 +0300

    besm allah

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,13 +9,13 @@ def canUnlockAll(boxes):
 
     n = len(boxes)
     unlocked = set([0])  # Start with the first box unlocked
-    keys = [0]           # Use a list as a queue to manage the boxes we can visit
-    
+    keys = [0]   # Use a list as a queue to manage the boxes we can visit
+
     while keys:
         current_box = keys.pop()
         for key in boxes[current_box]:
             if key not in unlocked and key < n:
                 unlocked.add(key)
                 keys.append(key)
-    
+
     return len(unlocked) == n
